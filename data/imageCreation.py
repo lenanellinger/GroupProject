5161ab64066ac3a823982b7874d0ecb56ae0b8ae
lenanellinger <51322403+lenanellinger@users.noreply.github.com>
2023-01-05 18:55:30 +0100

    data

--- data/imageCreation.py
+++ data/imageCreation.py
@@ -1,11 +1,12 @@
 import numpy as np
+import os
 import itertools
 from matplotlib import pyplot as plt
 from Bio import SeqIO
 
 def getSequences(filePath):
     """
-
+    returns a list of sequences from specific file path
     :return: list of sequences
     """
     sequences = []
@@ -49,30 +50,56 @@ def convertMatrixToImage(pMatrix, intronExon, index, level):
     Converts Percentage Matrix To Image Of Size 875x656x3
 
     :param pMatrix: percentage Matrix
-    :return: image (matrix size 875x656x3)
+    :param intronExon: if intron or exon
+    :param index: index of image
+    :param level
+    :return: image
     """
     n = int(64/pMatrix.shape[0])
     image = np.kron(pMatrix, np.ones((n, n)))
-    path = "level" + str(level) + "/" + str(intronExon) + "/test" + str(index) + ".png"
-    plt.imsave(path, image, cmap='cividis')
+
+    # change colormap:
+    # - Greys
+    # perceptually uniform colormap (best choice in many applications)
+    # - cividis
+    # - viridis
+    # - plasma
+    # - inferno
+    # - magma
+    color_map = "inferno"
+    if intronExon == "mixed":
+        path = "mixed/" + color_map + "/level" + str(level) + "/" + str(intronExon)
+    else:
+        path = color_map + "/level" + str(level) + "/" + str(intronExon)
+    file_name = "image" + str(index) + ".png"
+
+    isExist = os.path.exists(path)
+    if not isExist:
+        os.makedirs(path)
+
+    plt.imsave(os.path.join(path, file_name), image, cmap=color_map)
 
 
-if __name__ == '__main__':
-    sequencesIntron = getSequences("introns.txt")
-    sequencesExon = getSequences("exon.txt")
+def generate_train_images():
+    sequencesIntron = getSequences("../dataGenerator/introns.txt")
+    sequencesExon = getSequences("../dataGenerator/exon.txt")
+
+    if len(sequencesExon) > len(sequencesIntron):
+        sequencesExon = sequencesExon[:len(sequencesIntron)]
+    else:
+        sequencesIntron = sequencesIntron[:len(sequencesExon)]
 
     for level in range(1, 7):
         alphabet = ['A', 'C', 'G', 'T']
         keywords = [''.join(i) for i in itertools.product(alphabet, repeat=level)]
         counter = 0
         for i in keywords:
-            print(i + " ", end = "")
+            print(i + " ", end="")
             counter += 1
             if counter == 16:
                 print()
                 counter = 0
-
-
+        print()
 
         for i, seq in enumerate(sequencesIntron):
             pMatrix = createPercentageMatrix(seq, level, keywords)
@@ -80,3 +107,20 @@ if __name__ == '__main__':
         for i, seq in enumerate(sequencesExon):
             pMatrix = createPercentageMatrix(seq, level, keywords)
             convertMatrixToImage(pMatrix, "exon", i, level)
+
+
+def generate_test_mixed_images():
+    mixedSequences = getSequences("../dataGenerator/1_exon_5000_length.txt")
+    level = 5
+
+    alphabet = ['A', 'C', 'G', 'T']
+    keywords = [''.join(i) for i in itertools.product(alphabet, repeat=level)]
+
+    for i, seq in enumerate(mixedSequences):
+        pMatrix = createPercentageMatrix(seq, level, keywords)
+        convertMatrixToImage(pMatrix, "mixed", i, level)
+
+
+if __name__ == '__main__':
+    generate_train_images()
+    # generate_test_mixed_images()
